fb0754a708de169ddba5b84c74bf450d8c321040
isbeji <3573919120@qq.com>
2026-02-01 21:06:07 +0800

    苍穹外卖初始代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,44 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    public Result<String> uplode(MultipartFile file){
+        log.info("文件上传：{}",file);
+        try {
+            String OriginalFilename = file.getOriginalFilename();
+            String extension = OriginalFilename.substring(OriginalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
+            String filePath = aliOssUtil.upload(file.getBytes(),objectName);
+            return Result.success(filePath);
+        } catch (Exception e) {
+            log.error("文件上传失败：{}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+
+
+}
