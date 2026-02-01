f555cbda481111e50bb1fe52decfb9b101cf46b3
isbeji <3573919120@qq.com>
2026-02-01 21:34:15 +0800

    苍穹外卖初始代码

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
 
 public interface DishService {
 
@@ -8,4 +11,9 @@ public interface DishService {
      * 新增菜品，同时保存对应的口味数据
      */
     void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
